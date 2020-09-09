dba4dfd57b116441d9287b33f0fda06d671f6a48
Ing. Emanuel Torres Clemente <emanueljtc@desarrollowebtotal.com>
2020-09-09 18:59:33 -0400

    sw

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -7,6 +11,32 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram 🐶',
+      description: 'Con Petgram puedes encontrar fotos de mascotas facilmente.',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve(__dirname, 'src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('Icons'),
+          ios: true
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        }
+      ]
     })
   ],
   module: {
